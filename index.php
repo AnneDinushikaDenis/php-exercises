7830c16dc5c8588ab4d9074041738719a576fb90
AnneDinushikaDenis <anndinushika@gmail.com>
2022-06-27 14:12:52 +0530

    Add styles

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Php-exercise-index</title>
+</head>
+<body>
+    
+<div class="container" style="background-color: rgb(30, 22, 139); color: bisque;" >
+
+<h2 style="text-align:center">Student Register Form</h2>
+<hr>
+<form style="margin-bottom:20px">
+  
+<div class="mb-3" class="form-group">
+    <label for="studentid" class="form-label">Student ID</label>
+    <input type="text" class="form-control" id="studentid"  placeholder="Enter Student ID" class=>
+  </div>
+  
+  <div class="mb-3" class="form-group">
+    <label for="firstname" class="form-label">First Name</label>
+    <input type="text" class="form-control" id="firstname" placeholder="Enter First Name" >
+  </div>
+  
+  <div class="mb-3" class="form-group">
+    <label for="lastname" class="form-label">Last Name</label>
+    <input type="text" class="form-control" id="lastname"  placeholder="Enter Last Name" >
+  </div>
+  
+  <div class="mb-3" class="form-group">
+    <label for="dob" class="form-label">Date Of Birth</label>
+    <input type="text" class="form-control" id="dob" placeholder="Enter Date Of Birth" >
+  </div>
+  
+  <div class="mb-3" class="form-group">
+    <label for="email" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="email1"  placeholder="Enter email" >
+  </div>
+  
+  <div class="mb-3" class="form-group">
+    <label for="department" class="form-label">Department</label>
+    <input type="text" class="form-control" id="department" placeholder="Enter Department" >
+  </div>
+   
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div class="container">
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+</body>
+</html>
